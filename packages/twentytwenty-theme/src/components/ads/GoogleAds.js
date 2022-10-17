828f8d62377885a876a27708ccc6a5a5c798d573
murli <murli@balgiumwebnet.com>
2022-10-17 12:33:56 +0530

    adblocker

--- packages/twentytwenty-theme/src/components/ads/GoogleAds.js
+++ packages/twentytwenty-theme/src/components/ads/GoogleAds.js
@@ -1,37 +1,39 @@
-import React, {useEffect,useState} from 'react';
-import {connect, styled} from "frontity";
+import React, { useEffect, useState } from 'react';
+import { connect, styled } from "frontity";
 import AdSense from 'react-adsense';
 
-const GoogleAds = ({actions}) => {
+const GoogleAds = ({ actions }) => {
     const [adblockerActive, setAdblockerActive] = useState(false);
 
     useEffect(() => {
         actions.source.fetch("/");
         const script = document.createElement("script");
-    
+
         script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
         script.async = true;
         script.onerror = (err) => err.type == "error" ? adBlockFunction() : "";
-    
+
         document.body.appendChild(script);
-      }, [actions.source]);
-      const adBlockFunction = () => {
+    }, [actions.source]);
+    const adBlockFunction = () => {
         // Google Analytics End
         setAdblockerActive(true);
-      }
+    }
 
     return (
         <>
-        {adblockerActive ? 
-        <h2>Please Deactive Adblocker Tool</h2>
-    :
-       <AdSense.Google
-              client='ca-pub-5442643109134129'
-              slot='5764423148'
-              style={{ width: 500, height: 300, float: 'left' }}
-              format=''
-            />}
-            </>
+            {adblockerActive ?
+                <div style={{width : "800px", height : "300px", position:"fixed", top:"30%", left:0 , right: 0}}>
+                    <h2>Please Deactive Adblocker Tool</h2>
+                </div>
+                : ""}
+            <AdSense.Google
+                client='ca-pub-5442643109134129'
+                slot='5764423148'
+                style={{ width: 500, height: 300, float: 'left' }}
+                format=''
+            />
+        </>
     );
 };
 
