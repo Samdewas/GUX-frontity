1fb02fb33c6ab7240865a0da4e9c9896caea8469
Samir <samir@belgiumwebnet.com>
2022-10-27 18:38:53 +0530

    hover new

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   "name": "ux",
   "state": {
     "frontity": {
-      "url": "https://graphicux.com/",
+      "url": "https://befonts.com/",
       "title": "Test Frontity Blog",
       "description": "WordPress installation for Frontity development"
     }
@@ -23,7 +23,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://graphicux.com/",
+          "url": "https://befonts.com/",
           "params": {
             "per_page": 14,
             "type": ["post", "page"],
