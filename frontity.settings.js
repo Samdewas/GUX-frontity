e382e2ce2d59179aada60dd86a092ebebc44a6fd
Samir <samir@belgiumwebnet.com>
2022-11-07 18:36:21 +0530

    ux

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   "name": "ux",
   "state": {
     "frontity": {
-      "url": "https://www.edivaldobrito.com.br/",
+      "url": "https://graphicux.com/",
       "title": "Download Free Photoshop Action and All Ui/Ux resources - GraphicUX",
       "description": "Download Free Photoshop Action &amp; All Ui/Ux resources"
     }
@@ -23,7 +23,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://www.edivaldobrito.com.br/",
+          "url": "https://graphicux.com/",
           "params": {
             "per_page": 12,
             "type": ["post", "page"],
